9ce8e4299667145992442f981335aadda73a2068
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-09-14 16:42:55 +0200

    index and root changed

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,62 +1,49 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import styled from 'styled-components'
+import * as React from "react";
+import {graphql} from "gatsby";
+import styled from "styled-components";
+import {Layout} from "../layout";
 
-// Layout
-import Layout from '../layout/index'
-
-// Components
-import Image from '../components/Image'
-import Heading from '../components/Heading'
-
-interface IndexPageProps {
+type IndexPageProps = {
   location: {
-    pathname: string
-  }
+    pathname: string;
+  };
   data: {
     image: {
       childImageSharp: {
-        fluid: any
-      }
-    }
+        fluid: any;
+      };
+    };
     site: {
       siteMetadata: {
-        title: string
-        description: string
-      }
-    }
-  }
-}
+        title: string;
+        description: string;
+      };
+    };
+  };
+};
 
 const Wrapper = styled.div`
   width: 100%;
-  min-height: 100vh;
+  min-height: 90vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: ${p => p.theme.spacing.unit * 3}px;
   background: #003580;
-`
+`;
 
-export default ({ data, location }: IndexPageProps) => {
-  const { image, site } = data
+export default ({data, location}: IndexPageProps) => {
+  const {image, site} = data;
   return (
     <Layout location={location}>
-      <Wrapper>
-        <Image img={image.childImageSharp} />
-        <Heading
-          title={site.siteMetadata.title}
-          subtitle={site.siteMetadata.description}
-        />
-      </Wrapper>
+      <Wrapper>asfjoasjofajosfjoasf</Wrapper>
     </Layout>
-  )
-}
+  );
+};
 
 export const indexPageQuery = graphql`
   query IndexPageQuery {
-    image: file(relativePath: { eq: "icon.png" }) {
+    image: file(relativePath: {eq: "icon.png"}) {
       ...fluidImage
     }
     site {
@@ -66,4 +53,4 @@ export const indexPageQuery = graphql`
       }
     }
   }
-`
+`;
