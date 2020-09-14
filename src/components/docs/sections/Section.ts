67c2353e2d4196b3e2dfcf124393dd1ac11285ae
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-09-14 17:32:50 +0200

    Create Section.ts

---
+++ src/components/docs/sections/Section.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import {H1, P} from "axelra-styled-bootstrap-grid/dist/content/Typography";
+import {Flex, SPACING} from "axelra-styled-bootstrap-grid";
+import {__GRAY_SCALE} from "../../../layout/theme";
+
+export const SectionTitle = styled(H1)``;
+
+export const SectionParagraph = styled(P)``;
+
+export const SectionBox = styled(Flex)`
+  border: 2px solid ${__GRAY_SCALE._200};
+  width: 100%;
+`;
+
+export const SectionLayout = styled(Flex)`
+  width: 100%;
+  padding: ${SPACING * 2}px;
+`;
+
+export const SectionCode = styled(Flex)`
+  background: ${__GRAY_SCALE._200};
+  width: 100%;
+  border-radius: 2px;
+  min-height: 40px;
+`;
